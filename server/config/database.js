8027a342749323ea536eee5dd7b2dc722f4d1b19
SongZheDerrick <song.zhe2@husky.neu.edu>
2017-08-13 17:39:57 -0700

    mongodb connection

--- server/config/database.js
+++ server/config/database.js
@@ -1,4 +1,6 @@
 // config/database.js
 module.exports = {
-    'url' : 'mongodb://heroku_test2:heroku_test2@ds163181.mlab.com:63181/heroku_6lpbqlfx'
+    'url' : 'mongodb://heroku_cz0t2rkb:4ohhj1rmijnh7jo3ngd6sd98i4@ds139665.mlab.com:39665/heroku_cz0t2rkb'
+    //'url' : 'mongodb://heroku_test2:heroku_test2@ds163181.mlab.com:63181/heroku_6lpbqlfx'
+
 };
